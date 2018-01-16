6d354bdfaaa9f088763ad74398b1de5096e82c1f
hoang tram <hoangtram56@gmail.com>
2018-01-16 14:38:21 +0700

    v3

---
+++ Cowell/Brand/Controller/Adminhtml/Index/NewAction.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Cowell\Brand\Controller\Adminhtml\Index;
+
+/**
+ * Description of NewAction
+ *
+ * @author Tram
+ */
+class NewAction extends \Magento\Backend\App\Action
+{
+    protected $resultForwardFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+    ) {
+        $this->resultForwardFactory = $resultForwardFactory;
+        parent::__construct($context, $coreRegistry);
+    }
+
+    /**
+     * New action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('edit');
+    }
+}
